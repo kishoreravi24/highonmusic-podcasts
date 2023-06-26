0f836bd5c47faf32ae49c01d9efc05a1ac89ddf9
kishore Jr <kishoreravi24399@gmail.com>
2023-06-26 11:36:05 +0530

    service registered

--- src/main/java/com/highonmusic/podcasts/controller/Controller.java
+++ src/main/java/com/highonmusic/podcasts/controller/Controller.java
@@ -1,13 +1,15 @@
 package com.highonmusic.podcasts.controller;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/podcasts")
+@EnableDiscoveryClient
 public class Controller {
-    @GetMapping("/")
+    @GetMapping("")
     public String test(){
         return "Hello, from podcasts microservice";
     }
